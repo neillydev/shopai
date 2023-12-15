f6cdd0f7b5c5cb1ae09d0a304743b2804f54a8ee
neillydev <neillydev@gmail.com>
2023-12-15 13:55:53 -0800

    update Hero elements and styling

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -5,11 +5,16 @@ import styles from './Hero.module.css';
 const Hero = () => {
   return (
     <section className={`${styles.heroSection} flex space-between`}>
-      <div className={`${styles.heroLeft}`}>
-        <h1 className={`${styles.heroHeader}`}>With Droppy, every product is a <span className={`${styles.special}`}>winning</span> product<span className={`${styles.special}`}>.</span></h1>
-        <h2 className={`${styles.heroSubHeader}`}>Our unique AI solution streamlines the process of testing products and creatives, enabling you to devote more energy to growing your business rather than getting bogged down in the details of testing.</h2>
+      <div className={`${styles.heroLeft} flex justify-center items-center`}>
+        <div className={`${styles.heroLeftWrapper}`}>
+          <h1 className={`${styles.heroHeader}`}>With Droppy, every product is a <span className={`${styles.special}`}>winning</span> product<span className={`${styles.special}`}>.</span></h1>
+          <h2 className={`${styles.heroSubHeader}`}>Our unique AI solution streamlines the process of testing products and creatives, enabling you to devote more energy to growing your business rather than getting bogged down in the details of testing.</h2>
+          <button className={`${styles.getStartedBtn}`}>Get Started</button>
+          <button className={`${styles.watchDemoBtn}`}>Watch Demo</button>
+        </div>
       </div>
-      <div className={`${styles.heroRight}`}>
+      <div className={`${styles.heroRight} flex justify-center items-center`}>
+        <div className={`${styles.lottieAnimationPlaceholder}`}></div>
       </div>
     </section>
   )
