a4d6ebf23059dc652fc7f197d23b0db17d01be85
neillydev <neillydev@gmail.com>
2023-12-15 13:53:04 -0800

    update layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Droppy AI',
+  title: 'Droppy Ai',
   description: 'Droppy: Dropshipping AI Specialist',
 }
 
