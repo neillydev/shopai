ab7c9195fa0cf884de50c2bafcaf4bef6a343da9
neillydev <neillydev@gmail.com>
2023-12-15 13:53:20 -0800

    add Loader functionality to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,28 @@
-
+'use client';
+import { useEffect, useState } from 'react';
 import Navigation from '@/components/Navigation/Navigation';
 import Hero from '@/components/Hero/Hero';
+import Loading from '@/components/Loading/Loading';
 import './globals.css';
 
+const loaderDuration = 1500;
+
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => setLoading(false), loaderDuration + 750);
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center overflow-x-hidden">
-      <Navigation />
-      <Hero />
-    </main>
+    <>
+      {loading ?
+        <Loading duration={loaderDuration} /> :
+        <main className="flex min-h-screen flex-col items-center overflow-x-hidden">
+          <Navigation />
+          <Hero />
+        </main>
+      }
+    </>
   )
 };
