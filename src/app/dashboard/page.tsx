879c2b56c7e766e0b9a79741a443db5d0df86b91
neillydev <neillydev@gmail.com>
2023-12-22 04:53:56 -0800

    create Dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,19 @@
+import { getServerSession } from 'next-auth/next';
+import { redirect } from 'next/navigation';
+import { options } from '../api/auth/[...nextauth]/options';
+
+import styles from './Dashboard.module.css';
+import Main from '@/components/Dashboard/Main';
+
+export default async function Dashboard() {
+  const session = await getServerSession(options);
+  if (!session) {
+    redirect('/auth?callbackUrl=/dashboard');
+  }
+
+  return (
+    <div className={`${styles.dashContainer} flex`}>
+      <Main />
+    </div>
+  )
+};
---
+++ src/components/Dashboard/Main.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const Main = () => {
+  const [loading, setLoading] = useState(true);
+  const [dashboardLoading, setDashboardLoading] = useState(true);
+  
+  return (
+    <></>
+  )
+}
+
+export default Main;
