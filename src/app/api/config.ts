a1e4ed9f809a055732ff4b81d9975b2ab66f632b
neillydev <neillydev@gmail.com>
2024-02-19 15:44:11 -0800

    update configuration

--- src/app/api/config.ts
+++ src/app/api/config.ts
@@ -1,4 +1,4 @@
 const isDevelopment = process.env.NODE_ENV === 'development';
-const baseURL = isDevelopment ? 'http://localhost:8000' : '';
+const baseURL = isDevelopment ? 'http://localhost:9000' : '';
 
 export { baseURL };
