f59618730d74a4b57b0d4012885361b31065dcbe
neillydev <neillydev@gmail.com>
2024-03-31 23:41:25 -0700

    fix API base url

--- src/app/api/config.ts
+++ src/app/api/config.ts
@@ -1,4 +1,4 @@
 const isDevelopment = process.env.NODE_ENV === 'development';
-const baseURL = isDevelopment ? 'http://localhost:9000' : '';
+const baseURL = 'http://localhost:9000';
 
 export { baseURL };
