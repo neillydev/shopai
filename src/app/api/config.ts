a542080a59be3bcefac646b3b408de6e0aaff687
neillydev <neillydev@gmail.com>
2024-04-01 01:31:16 -0700

    fix API base url

--- src/app/api/config.ts
+++ src/app/api/config.ts
@@ -1,4 +1,4 @@
 const isDevelopment = process.env.NODE_ENV === 'development';
-const baseURL = isDevelopment ? 'http://localhost:9000/api' : 'http://droppy.ai/api/';
+const baseURL = isDevelopment ? 'http://localhost:9000/api' : 'http://droppy.ai/api';
 
 export { baseURL };
